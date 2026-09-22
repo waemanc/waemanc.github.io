6eaf855c451e82890cb968a31a9e1c2761c7d469
waemanc <cwaeman@gmail.com>
2026-09-22 14:06:52 -0400

    Added logic to the take damage function

--- day7/script.js
+++ day7/script.js
@@ -7,6 +7,15 @@ function takeDamage() {
     const healthText = document.getElementById("health-display");
     const statusText = document.getElementById("status-message");
 
-    healthText.innerText = playerHealth; 
+    if(playerHealth > 0)
+    {
+        healthText.innerText = playerHealth; 
+        statusText.innerText = "You've been hit!";
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Game Over!";
+    }
 }
 
