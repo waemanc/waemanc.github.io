fbe6a754ea106f65ab11d2e56e404d58e5bc0785
waemanc <cwaeman@gmail.com>
2026-09-24 13:21:19 -0400

    added to main page lab 5

---
+++ day7 copy/script.js
@@ -0,0 +1,28 @@
+let playerHealth = 100;
+const damageAmount = 30;
+
+function takeDamage() {
+    playerHealth = playerHealth - damageAmount;
+
+    const healthText = document.getElementById("health-display");
+    const statusText = document.getElementById("status-message");
+
+    if(playerHealth > 0)
+    {
+        healthText.innerText = playerHealth; 
+        statusText.innerText = "You've been hit!";
+    }
+    else
+    {
+        healthText.innerText = 0;
+        statusText.innerText = "Game Over!";
+        statusText.style.color = "#ff432e";
+        statusText.style.fontWeight = "bold";
+
+        document.body.style.backgroundColor = "#5a1a1a";
+
+        document.querySelector("button").disabled = true;
+        document.querySelector("button").innerText = "Dead";
+    }
+}
+
