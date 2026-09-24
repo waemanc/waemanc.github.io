da096252af291a7b6ba20e5782b94720d0e99b21
waemanc <cwaeman@gmail.com>
2026-09-24 14:05:34 -0400

    changes to java script for lab 5 for add money and subtract money

--- lab5/script.js
+++ lab5/script.js
@@ -1,28 +1,43 @@
-let playerHealth = 100;
-const damageAmount = 30;
+let bankTotal = 100;
+const addMoney = 25;
+const subtractMoney = 25;
 
-function takeDamage() {
-    playerHealth = playerHealth - damageAmount;
+function addMoney() {
+    bankTotal = bankTotal + addMoney;
 
-    const healthText = document.getElementById("health-display");
+    const depositText = document. getElementById("money-display");
     const statusText = document.getElementById("status-message");
 
-    if(playerHealth > 0)
+    if(bankTotal > 0)
     {
-        healthText.innerText = playerHealth; 
-        statusText.innerText = "You've been hit!";
+        depositText.innerText = bankTotal;
+        statusText.innerText = "Deposited Money!";
     }
+
+}
+
+function subtractMoney() {
+    bankTotal = bankTotal - subtractMoney;
+
+    const depositText = document. getElementById("money-display");
+    const statusText = document.getElementById("status-message");
+
+    if(bankTotal > 0)
+    {
+        depositTextText.innerText = bankTotal;
+        statusText.innerText = "Withdrew Money!";
+    }
+
     else
     {
-        healthText.innerText = 0;
-        statusText.innerText = "Game Over!";
-        statusText.style.color = "#ff432e";
+        depositTextText.innerText = 0;
+        statusText.innerText = "Uh Oh Bankrupt";
+        statusText.style.color = "#c91804";
         statusText.style.fontWeight = "bold";
 
         document.body.style.backgroundColor = "#5a1a1a";
 
         document.querySelector("button").disabled = true;
-        document.querySelector("button").innerText = "Dead";
+        document.querySelector("button").innerText = "Bankrupt";
     }
 }
-
